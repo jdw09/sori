8429ffd1bcf49f6af07fbc44f178ed86cad5fc24
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-06-09 20:39:41 +0900

    초기 커밋

---
+++ src/main/java/bxnd/sori/SoriApplication.java
@@ -0,0 +1,13 @@
+package bxnd.sori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bxnd/sori/SoriApplicationTests.java
@@ -0,0 +1,13 @@
+package bxnd.sori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
