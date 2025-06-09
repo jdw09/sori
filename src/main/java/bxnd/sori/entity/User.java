8bf46243d0e0d464a471d45353d60f3a398f369f
ParkJunSeok <jdksla0129@gmail.com>
2025-06-10 00:17:25 +0900

    Student-C

--- src/main/java/bxnd/sori/entity/User.java
+++ src/main/java/bxnd/sori/entity/User.java
@@ -15,6 +15,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+  private String password;
+  private String role;
     @Column(nullable = false, unique = true)
     private String username;
 
