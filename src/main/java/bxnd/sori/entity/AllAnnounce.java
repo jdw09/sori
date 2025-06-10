aa2b49a9b0c293c5e67778d0d5df198ce4d76907
jdw09 <razion0304@naver.com>
2025-06-10 19:28:39 +0900

    Entity생성

---
+++ src/main/java/bxnd/sori/entity/AllAnnounce.java
@@ -0,0 +1,30 @@
+package bxnd.sori.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "all_announce") // 테이블명 명시
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class AllAnnounce extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id") // PK 컬럼명 매핑
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private Member author;
+}
---
+++ src/main/java/bxnd/sori/entity/Assignment.java
@@ -0,0 +1,34 @@
+package bxnd.sori.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Assignment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "assignment_id") // PK 컬럼명 매핑
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime deadline; // java.sql.Date -> LocalDateTime
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false) // 관계 추가
+    private Member author;
+}
